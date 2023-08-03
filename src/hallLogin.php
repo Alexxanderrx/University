a11e7b8f03fe13a2c132c681cbc446c34e7a839d
Alexxanderrx <alexmorales196@gmail.com>
2023-08-02 20:02:05 -0500

    Comienzo del proyecto

---
+++ src/hallLogin.php
@@ -0,0 +1,98 @@
+<?php
+session_start();
+require("connection.php");
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    //si es POST inicia secion con email y pass
+    $_SESSION["email_Lg"] = $_POST["emailLg"];
+    $_SESSION["pass_Lg"] = $_POST["passLg"];
+
+    $eLG = $_SESSION["email_Lg"];
+    $pLG = $_SESSION["pass_Lg"];
+
+    $resultLog = $mysqli->query("SELECT * FROM users WHERE email = '$eLG' AND password ='$pLG'");
+    $datosLog = $resultLog->fetch_assoc();
+
+    $resultLog = $mysqli->query("SELECT * FROM users WHERE email = '$eLG' AND password ='$pLG'");
+    $datosLog = $resultLog->fetch_assoc();
+
+    $datosLogRow = $resultLog->num_rows;
+
+    if ($datosLogRow == 1) {
+        if ($datosLog["id_rol"] == 1) {
+            $_SESSION["cargo"]="Administrador";
+            // $_SESSION[""]=;
+            // $_SESSION[""]=;
+            // $_SESSION[""]=;
+            // $show = "SELECT * FROM user WHERE email = '$eLG' AND password = '$pLG' AND rol = 1";
+        } elseif ($datosLog["id_rol"] == 2) {
+            $show = "SELECT * FROM user WHERE email = '$eLG' AND password = '$pLG' AND rol = 2";
+        } elseif ($datosLog["id_rol"] == 3) {
+            $show = "SELECT * FROM user WHERE email = '$eLG' AND password = '$pLG' AND rol = 3";
+        }
+    } else {
+        $_SESSION["message_error"] = "Usuario o contraseña incorrectos.";
+        die();
+    }
+
+    // if ($datosVery["rol"] == "admin") {
+    //     $show = "SELECT * FROM user WHERE email = '$eLG' AND password = '$pLG' AND rol = 1";
+    // } elseif ($datosVery["rol"] == "mod") {
+    //     $show = "SELECT * FROM user WHERE email = '$eLG' AND password = '$pLG' AND rol = 2";
+    // } elseif ($datosVery["rol"] == "empleado") {
+    //     $show = "SELECT * FROM user WHERE email = '$eLG' AND password = '$pLG' AND rol = 3";
+    // }
+
+    // if (password_verify($pLG, $datosVery["password"])) {
+
+    // $show = "SELECT * FROM user WHERE email = '$eLG'";
+    // } else {
+    // $show = "SELECT * FROM user WHERE email = '$eLG' AND password = '$pLG'";
+    //     $show = "SELECT * FROM user WHERE email = '$eLG' AND password = '$pLG' AND rol ='admin'";
+    // }
+} else {
+    //si NO es POST inicia secion con id
+    // $dId = $_SESSION["info_id"];
+    // $show = "SELECT * FROM users WHERE id = '$dId';";
+}
+
+// $result = $mysqli->query($show);
+// $datos = $result->fetch_assoc();
+// $numFilas = $result->num_rows;
+
+
+
+// if ($numFilas === 1) {
+//     header("Location: lobbyAdmin.php");
+// } else {
+//     $_SESSION["error_lg"] = "Wrong email or password.";
+//     header("Location: login.php");
+//     die();
+// }
+
+// $_SESSION["datos"] = $datos;
+// $_SESSION["info_id"] = $_SESSION["datos"]["id_info"];
+// $ssId = $_SESSION["info_id"];
+
+// $showId = "SELECT * FROM users_admin WHERE id_info = '$ssId'";
+
+// $resultId = $mysqli->query($showId);
+// $datosId = $resultId->fetch_assoc();
+// $_SESSION["datosId"] = $datosId;
+
+// $numFilasId = $resultId->num_rows;
+
+// $_SESSION["info_photo"] = $_SESSION["datosId"]["photo"];
+// $_SESSION["info_name"] = $_SESSION["datosId"]["name"];
+// $_SESSION["info_bio"] = $_SESSION["datosId"]["bio"];
+// $_SESSION["info_phone"] = $_SESSION["datosId"]["phone"];
+// $_SESSION["info_email"] = $_SESSION["datosId"]["email"];
+// $_SESSION["info_password"] = $_SESSION["datosId"]["password"];
+// if ($numFilasId === 1) {
+
+//     header("Location: lobyAdmin.php");
+// } else {
+//     $_SESSION["error_lg"] = "Wrong email or password.";
+//     header("Location: login.php");
+//     die();
+// }
---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+const toggleBar = document.getElementById("toggle");
+
+const toggleSpin = document.getElementById("spin");
+
+const btnOpen = document.getElementById("open");
+
+let spin = false;
+
+btnOpen.addEventListener("click", () => {
+    toggleBar.classList.toggle("collapse");
+    spin = (!spin);
+    // console.log(spin);
+    if (spin == true) {
+        toggleSpin.style.transform = "rotate(180deg)";
+    } else if (spin == false) {
+        toggleSpin.style.transform = "rotate(0deg)";
+    }
+});
+
+const toggleMenu = document.getElementById("toggleMenu");
+const toggleMenuU = document.getElementById("toggleMenuU");
+const CARGO = document.getElementById("CARGO");
+const Uni = document.getElementById("Uni");
+const MENU_AD = document.getElementById("MENU_AD");
+const pPermisos = document.getElementById("pPermisos");
+const pMaestros = document.getElementById("pMaestros");
+const pAlumnos = document.getElementById("pAlumnos");
+const pClases = document.getElementById("pClases");
+const section01 = document.getElementById("section01");
+
+const btnTree = document.getElementById("btnOpen");
+
+btnTree.addEventListener("click", () => {
+    section01.classList.toggle("w-4/5");
+    section01.classList.toggle("w-full");
+    toggleMenu.classList.toggle("w-1/5");
+    toggleMenu.classList.toggle("w-[250px]");
+    toggleMenuU.classList.toggle("md:justify-start");
+    toggleMenuU.classList.toggle("md:pl-4");
+    toggleMenuU.classList.toggle("justify-center");
+    CARGO.classList.toggle("md:flex");
+    Uni.classList.toggle("md:flex");
+    MENU_AD.classList.toggle("md:flex");
+    pPermisos.classList.toggle("md:flex");
+    pMaestros.classList.toggle("md:flex");
+    pAlumnos.classList.toggle("md:flex");
+    pClases.classList.toggle("md:flex");
+});
